e7f2921ce2a1efc5249fbf04111e79bf95fe7590
nadia-83 <nadinmamamia@gmail.com>
2020-05-17 13:21:56 -0500

    it is my second commit

--- HomworkABCDE.js
+++ HomworkABCDE.js
@@ -3,6 +3,7 @@ let expectedWord = "CDEAB";
 let midlWord;
 console.log(firstWord.length);
 let lengthOfFirstWord=firstWord.length;
+
 for (let i = 0; i < lengthOfFirstWord; i++) {
     console.log(i);
     midlWord=firstWord.slice(1,5) + firstWord.charAt(0);
