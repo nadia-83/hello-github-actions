2d90fefe206a80797052c552d7f98914654015e7
nadia-83 <nadinmamamia@gmail.com>
2020-06-07 12:31:49 -0500

    Please enter the commit message for your changes. Lines starting My first commit message

---
+++ ifElsePractics.js
@@ -0,0 +1,89 @@
+/*let a = 0;
+if (a>=0) {console.log ('true!');} else {console.log ('fals');}
+console.log(a);
+if (a==0) {console.log('true');} else {console.log ('fals');}
+console.log(a);
+
+if (a==0) console.log('true'); else console.log ('fals');
+console.log(a);
+if (a=1) console.log ('true'); else console.log ('fals');
+let b = 3;
+let c = 0;
+if (b > 0 && c < 0) console.log('true!'); else console.log ('fals!');
+console.log(c); 
+let d = -5;
+let e = 6;
+if ( d > 0|| e < 0) console.log ('true!!'); else console.log ('false!!');
+let f = true;
+if (f == true) console.log('true!'); else console.log('false');
+console.log(f);
+let g = true;
+if (g) console.log('true'); else console.log('false');
+console.log(g);
+if (g === undefined) 
+{ console.log ('g!!');
+} else if (g>0) {
+    console.log ('More zero!!');
+} else {console.log ('Litl zero!!');
+} 
+let h = 0;
+if (h == -3) {
+    console.log ('True!');
+ } else {
+     console.log ('False!');
+ }
+console.log(h);
+let lang 
+switch(lang) {
+    case 'ru': console.log('Привет');
+    break;
+    case 'en': console.log('Hello');
+    break; 
+    default: console.log('Not and');
+    break;
+
+
+
+}
+console.log(lang);
+
+let num = 2 - 1
+switch(num) {
+    case 1: console.log('Winter');
+    break;
+    case 2: console.log('Spring');
+    break;
+    case 3: console.log('Summer');
+    break;
+    default: console.log('Not and');
+
+
+}
+console.log(num);*/
+
+// let day = 30;
+// if (day >= 0 && day <= 10) 
+// { console.log ('first');
+// }
+// if (day>= 11 && day <= 20 ) 
+// { console.log('second');
+// } 
+// if (day >= 21 && day <=31) 
+// {console.log('third');
+// }
+// console.log(day);
+
+let month = 0;
+if (month == 12 || month <= 2 )
+{console.log ('winter');
+}
+if (month >= 3 && month <= 5 )
+{console.log ('spring');
+}
+if (month >= 6 && month <= 8 )
+{console.log ('Summer');
+}
+if (month >=  9 && month <= 11)
+{console.log ('autum');
+}
+console.log (month);
